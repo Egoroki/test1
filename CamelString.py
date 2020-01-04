7c7cfd0679619a726e4a5168d3fffeb63f3b757c
ypalatau@gmail.com <Egorokio1>
2020-01-04 13:03:06 +0300

    New changelist

---
+++ CamelString.py
@@ -0,0 +1,20 @@
+def to_camel_case(text):
+    output = ''.join(x for x in text.title() if x not in '-' '_')
+    try:
+        return text[0] + output[1:]
+    except Exception:
+        return ''
+
+
+    # result = []
+    # counter = 0
+    # for word in text:
+    #     if word not in '-' '_':
+    #         result.append(word)
+    #         counter += 1
+    #     else
+    # return result
+    # # return str(''.join(result))
+
+
+print(to_camel_case("в_-ыаыва"))
